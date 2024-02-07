9dd1d7663897ceb82b8d822247ac4272f75a3f78
shaikkutagalsameer <shaikkutagalsameer207@email.com>
2024-02-07 22:34:40 +0530

    files of portfolio

---
+++ portfolio/app.js
@@ -0,0 +1,12 @@
+var typed=new Typed(".auto-type",{
+    strings:["Frontend Developer"],
+    typeSpeed:150,
+    backSpeed:150,
+    loop:true
+});
+ function getabout(){
+    window.location.href="#about";
+
+
+ };
+    
